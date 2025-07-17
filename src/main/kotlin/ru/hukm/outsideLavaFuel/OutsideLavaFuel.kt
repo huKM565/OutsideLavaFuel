a75d8deec4251dfcd9f62644fe535ebefb245ab3
hukm <gornih.ilya@gmail.com>
2025-07-17 23:56:30 +0500

    Initial commit

---
+++ src/main/kotlin/ru/hukm/outsideLavaFuel/OutsideLavaFuel.kt
@@ -0,0 +1,14 @@
+package ru.hukm.outsideLavaFuel
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OutsideLavaFuel : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
